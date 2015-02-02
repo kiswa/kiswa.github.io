056fe06bf2de806cf986b5d6b4f7ae38dbddc846
kiswa <kiswa.com@gmail.com>
2015-02-02 15:28:02 -0500

    Include Bitbucket projects in display.

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -4,7 +4,7 @@ myApp.controller('MainCtrl', [
         $scope.bbRepos = [];
         $scope.repos = [];
         $scope.readme = {
-            text: 'Click the magnifying glass on a project and the README will be displayed here.'
+            text: 'Click the magnifying glass on a GitHub project and the README will display here.'
         };
 
         $scope.getReadMe = function(repo) {
@@ -12,6 +12,45 @@ myApp.controller('MainCtrl', [
             .success(function(data) {
                 $scope.readme.text = marked(data);
             });
+        };
+
+        $scope.loadIssues = function() {
+            for (var i = 0; i < $scope.bbRepos.length; ++i) {
+                var repoName = $scope.bbRepos[i].full_name.split('/')[1];
+                $scope.bbRepos[i].open_issues_count = "N/A";
+
+                if ($scope.bbRepos[i].has_issues) {
+                    BitbucketService.getIssues(repoName)
+                    .success(function(data, status, headers, config) {
+                        var name = config.url.split('/')[6];
+                        for (var i = 0; i < $scope.bbRepos.length; ++i) {
+                            if ($scope.bbRepos[i].full_name.split('/')[1] == name) {
+                                $scope.bbRepos[i].open_issues_count = data.count;
+                            }
+                        }
+                    });
+                }
+            }
+        };
+
+        $scope.loadWatchers = function() {
+            for (var i = 0; i < $scope.bbRepos.length; ++i) {
+                var repoName = $scope.bbRepos[i].full_name.split('/')[1];
+                $scope.bbRepos[i].watchers_count = 0;
+
+                if ($scope.bbRepos[i].has_issues) {
+                    BitbucketService.getWatchers(repoName)
+                    .success(function(data, status, headers, config) {
+                        console.log(data);
+                        var name = config.url.split('/')[6];
+                        for (var i = 0; i < $scope.bbRepos.length; ++i) {
+                            if ($scope.bbRepos[i].full_name.split('/')[1] == name) {
+                                $scope.bbRepos[i].watchers_count = data.size;
+                            }
+                        }
+                    });
+                }
+            }
         };
 
         GitHubService.getRepos()
@@ -22,6 +61,8 @@ myApp.controller('MainCtrl', [
         BitbucketService.getRepos()
         .success(function(data) {
             $scope.bbRepos = data.values;
-        })
+            $scope.loadIssues();
+            $scope.loadWatchers();
+        });
     }
 ]);
--- js/services/Bitbucket.js
+++ js/services/Bitbucket.js
@@ -1,11 +1,21 @@
 myApp.factory('BitbucketService', [
     '$http',
     function($http) {
-        var url = 'https://bitbucket.org/api/2.0/';
+        var urlv2 = 'https://api.bitbucket.org/2.0',
+            urlv1 = 'https://api.bitbucket.org/1.0',
+            callback = '?callback=JSON_CALLBACK';
 
         return {
             getRepos: function() {
-                return $http.get(url + 'repositories/kiswa');
+                return $http.jsonp(urlv2 + '/repositories/kiswa' + callback);
+            },
+
+            getIssues: function(repo) {
+                return $http.jsonp(urlv1 + '/repositories/kiswa/' + repo + '/issues/' + callback + '&status=open');
+            },
+
+            getWatchers: function(repo) {
+                return $http.jsonp(urlv2 + '/repositories/kiswa/' + repo + '/watchers' + callback);
             }
         };
     }
